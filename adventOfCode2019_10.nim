f830b64e0547b7e5c6ccdd548c587d9ca0a3fe1b
Luca Guzzon <luca.guzzon@gmail.com>
2019-12-10 16:08:53 +0100

    Almost perfect

--- adventOfCode2019_10.nim
+++ adventOfCode2019_10.nim
@@ -12,56 +12,6 @@ type
     y: int
 
 const
-  gcInputTest00 = """......#.#.
-#..#.#....
-..#######.
-.#.#.###..
-.#..#.....
-..#....#.#
-#..#....#.
-.##.#..###
-##...#..#.
-.#....####"""
-  gcInputTest01 = """#.#...#.#.
-.###....#.
-.#....#...
-##.#.#.#.#
-....#.#.#.
-.##..###.#
-..#...##..
-..##....##
-......#...
-.####.###."""
-  gcInputTest02 = """.#..#..###
-####.###.#
-....###.#.
-..###.##.#
-##.##.#.#.
-....###..#
-..#.#..#.#
-#..#.#.###
-.##...##.#
-.....#.#.."""
-  gcInputTest03 = """.#..##.###...#######
-##.############..##.
-.#.######.########.#
-.###.#######.####.#.
-#####.##.#.##.###.##
-..#####..#.#########
-####################
-#.####....###.#.#.##
-##.#################
-#####.##.###..####..
-..######..##.#######
-####.##.####...##..#
-.#####..#.######.###
-##...#.##########...
-#.##########.#######
-.####.#.###.###.#.##
-....##.##.###..#####
-.#.#.###########.###
-#.#.#.#####.####.###
-###.##.####.##.#..##"""
   gcInputOrig = """#...##.####.#.......#.##..##.#.
 #.##.#..#..#...##..##.##.#.....
 #..#####.#......#..#....#.###.#
@@ -94,12 +44,9 @@ const
 .#...#....#...##..##.#.#......#
 #..###.....##.#.......#.##...##"""
   gcInput = gcInputOrig
-  # gcInput = gcInputTest03
-
   gcInputRows = gcInput.split('\n')
   gcYCount = gcInputRows.len
   gcXCount = gcInputRows[0].len
-  gcCount = gcXCount.max(gcYCount)
 
 template getMeteors(): HashSet[Point] =
   block getMeteors:
@@ -110,11 +57,8 @@ template getMeteors(): HashSet[Point] =
           result.incl((lX, lY))
     result
 
-
 const
   gcMeteors = getMeteors()
-
-echo gcXCount, '-', gcYCount
 
 proc partOne =
   var lCounts: CountTable[Point]
@@ -141,27 +85,23 @@ proc partOne =
           lCurrent = (lCurrent.x + lDelta.x, lCurrent.y + lDelta.y)
   echo "partOne ", lCounts.largest
 
-
-
 proc partTwo =
   let lBase: Point = (17, 22)
-  # let lBase: Point = (11, 13)
   var lOthers = gcMeteors
   lOthers.excl(lBase)
-  var lDeltas = toSeq(lOthers.map(proc (it: Point): Point =
-    var lDelta: Point = (it.x - lBase.x, it.y-lBase.y)
-    let lGCD = lDelta.x.gcd(lDelta.y)
-    (lDelta.x div lGCD, lDelta.y div lGCD)).items).mapIt((x: it.x, y: it.y,
-        pa: block pseudoAngle:
-              let p = it.y.toFloat/(abs(it.x)+abs(it.y)).tofloat
-              if it.x >= 0:
-                p-1
-              else:
-                1-p
+  let lDeltas = toSeq(lOthers.map(proc (it: Point): Point =
+                                    let lDelta: Point = (it.x - lBase.x, it.y-lBase.y)
+                                    let lGCD = lDelta.x.gcd(lDelta.y)
+                                    (lDelta.x div lGCD, lDelta.y div lGCD)
+  ).items).mapIt(
+      (x: it.x,
+       y: it.y,
+       pa: block pseudoAngle:
+             let p = it.y.toFloat/(abs(it.x)+abs(it.y)).tofloat
+             if it.x >= 0: p-1 else: 1-p
   ))
   let lSortedDeltas = lDeltas.sortedByIt(it.pa)
   var lCounter = 0
-  var lResult: Point
   block searchOthers:
     while lOthers.len > 0:
       for lDelta in lSortedDeltas:
@@ -173,19 +113,14 @@ proc partTwo =
             (lCurrent.y >= gcYCount)):
           if not lOthers.missingOrExcl(lCurrent):
             lCounter.inc
-            # echo lCounter, " --> ", lCurrent
             if 200 == lCounter:
-              lResult = lCurrent
+              echo "partTwo ", lCurrent
               break searchOthers
             if 0 == lOthers.len:
               break searchOthers
             break
           lCurrent = (lCurrent.x + lDelta.x, lCurrent.y + lDelta.y)
 
-  # echo lDeltas
-  # echo lSortedDeltas
-
-  echo "partTwo ", lResult
 
 partOne() #288 --> (key: (x: 17, y: 22), val: 288)
 partTwo() #616 --> (x: 6, y: 16)
